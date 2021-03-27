eeb4496ea3fbe40085de5af5c49b491d1bf7aa3a
username <davidkinyanjui052@gmail.com>
2021-03-27 22:48:33 +0300

    Worked on Conditions

---
+++ conditioning2.py
@@ -0,0 +1,15 @@
+#<, >, ==, !=
+#Less than
+print(1<5)#Returns True
+print(5<1)#False
+#Greater than
+print(1>5)#False
+print(5>1)#True
+#Equal to
+print('jane'=='jane')#True
+print('jane'=="john")#False
+#Not Equal to
+print(5!=6)#True
+print(5!=5)#False
+print(False==True)#False
+print(1+2+2==5)#True
