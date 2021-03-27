838d556ce85c9cf745a193811b0477957a9f7be5
username <davidkinyanjui052@gmail.com>
2021-03-27 23:01:54 +0300

    Done with the basics of conditions

---
+++ Decisions.py
@@ -0,0 +1,15 @@
+# age=eval(input("Enter your age:"))
+age=12
+if age>=16:
+    print('Hey you are 16.')
+else:
+    print("Hey you are below 16.")
+
+# height=input()
+height='1'
+if eval(height)<1:
+    print('you can not ride.')
+elif eval(height)>2:
+    print("You cannot ride over 2 meters.")
+else:
+    print("You can ride.")
