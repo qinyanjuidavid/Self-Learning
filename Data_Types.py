86ad144d3072821f0dc397f37f8c41f1f2790d41
username <davidkinyanjui052@gmail.com>
2021-03-27 19:50:50 +0300

    Hands on Data Types & Variables

---
+++ Data_Types.py
@@ -0,0 +1,40 @@
+#Integers
+print(1)
+print(1000)
+print(type(1))
+#str
+print('Hello There')
+print("56")
+print(type("Hello There"))
+#Float
+print(3.14)
+print(36.6)
+print(type(3.14))
+#Boolean
+print(True)
+print(False)
+print(type(True))
+
+#Variables
+x=5#Declaring integer
+print(x)
+name="John Doe"#Declaring string
+print(name)
+pie=3.14#Declaring float
+print(pie)
+is_salty=True#Declaring Boolean
+print(is_salty)
+age=23
+print(age)
+_first_name="Jane Doe"
+print(_first_name)
+_=56
+print(_)
+age=weight=height=26
+print(age)
+print(weight)
+print(height)
+name,age,department="Jamie",32,"Human Resource"
+print(name)
+print(age)
+print(department)
