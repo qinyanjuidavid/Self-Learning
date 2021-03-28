9d546fb523d4f87fb686d35e6791cfee11f66c31
username <davidkinyanjui052@gmail.com>
2021-03-28 09:01:19 +0300

    Worked on basics of  functions

---
+++ HandsOnFuncs.py
@@ -0,0 +1,16 @@
+def addTwo(x):
+    return x+2
+print(addTwo(1))
+
+def subTwo(number):
+    return number-2
+print(subTwo(7))
+print(addTwo(6))
+def printString(string):
+    print(string)
+printString("Hello")
+
+def accel(mass,force):
+    a=mass*force
+    return a
+print(accel(20,5))
