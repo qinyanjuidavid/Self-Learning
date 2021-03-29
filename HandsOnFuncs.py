2be30b49ecbabe83aa0ae4317e5a95fdcc802348
username <davidkinyanjui052@gmail.com>
2021-03-29 20:03:30 +0300

    Hands on Arguments

--- HandsOnFuncs.py
+++ HandsOnFuncs.py
@@ -19,3 +19,18 @@ def doSomething():
     print("Hello world")
     print("My name is day.")
 doSomething()
+#Recarp
+def func(x,text):
+    print(x)
+    if text=="1":
+        print("text is 1")
+    else:
+        print("text is not one")
+func(5,'1')
+#Argument or Adding the default value
+def newFunct(n=10,y="John Doe"):
+    print(f"My name is {y}.")
+    return "this is number {}".format(n)
+print(newFunct()) #With arguments its not a must for us to put values when calling the function
+print(newFunct(19,"Jane Doe")) #We can change the default value
+print(newFunct(9))
